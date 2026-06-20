3cb038cf2d022a2230bbb13e87b79e92c1abeade
Duddle <108181248+DUDDLEGOD@users.noreply.github.com>
2026-06-20 11:23:49 +0530

    build: simplify build platform configuration and force mingw32 compiler for Windows builds

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 import sys
 from setuptools import setup, Extension, find_packages
+from setuptools.command.build_ext import build_ext
+
+class my_build_ext(build_ext):
+    def finalize_options(self):
+        super().finalize_options()
+        if sys.platform == 'win32':
+            self.compiler = 'mingw32'
 
 include_dirs = ['src']
 library_dirs = []
@@ -7,25 +14,7 @@ libraries = []
 extra_compile_args = ['-std=c99']
 extra_link_args = []
 
-if sys.platform.startswith('linux'):
-    include_dirs.append('/usr/local/include')
-    library_dirs.append('/usr/local/lib')
-    extra_link_args.extend([
-        '-Wl,-Bstatic', '-lraylib', '-Wl,-Bdynamic',
-        '-lGL', '-lm', '-lpthread', '-ldl', '-lrt', '-lX11'
-    ])
-elif sys.platform == 'darwin':
-    include_dirs.append('/usr/local/include')
-    library_dirs.append('/usr/local/lib')
-    extra_link_args.extend([
-        '/usr/local/lib/libraylib.a',
-        '-framework', 'OpenGL',
-        '-framework', 'Cocoa',
-        '-framework', 'IOKit',
-        '-framework', 'CoreAudio',
-        '-framework', 'CoreVideo'
-    ])
-elif sys.platform == 'win32':
+if sys.platform == 'win32':
     include_dirs.append(r'C:\raylib\include')
     library_dirs.append(r'C:\raylib\lib')
     libraries.extend(['raylib', 'gdi32', 'winmm', 'user32', 'shell32'])
@@ -43,4 +32,8 @@ doodle_module = Extension(
 setup(
     packages=find_packages(),
     ext_modules=[doodle_module],
+    cmdclass={
+        'build_ext': my_build_ext,
+    },
 )
+
